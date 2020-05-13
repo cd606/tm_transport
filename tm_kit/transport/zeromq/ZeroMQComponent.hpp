6cac6f3edcb6dc89fd4115f06f9609ccd08e8f46
cd606 <64788159+cd606@users.noreply.github.com>
2020-05-13 16:03:42 -0500

    add zeromq pub/sub

---
+++ tm_kit/transport/zeromq/ZeroMQComponent.hpp
@@ -0,0 +1,38 @@
+#ifndef TM_KIT_TRANSPORT_ZEROMQ_ZEROMQ_COMPONENT_HPP_
+#define TM_KIT_TRANSPORT_ZEROMQ_ZEROMQ_COMPONENT_HPP_
+
+#include <memory>
+#include <functional>
+#include <optional>
+#include <regex>
+#include <variant>
+#include <string>
+
+#include <tm_kit/infra/WithTimeData.hpp>
+#include <tm_kit/basic/ByteData.hpp>
+#include <tm_kit/transport/ConnectionLocator.hpp>
+#include <tm_kit/transport/ByteDataHook.hpp>
+
+namespace dev { namespace cd606 { namespace tm { namespace transport { namespace zeromq {
+    
+    class ZeroMQComponentImpl;
+
+    class ZeroMQComponent {
+    private:
+        std::unique_ptr<ZeroMQComponentImpl> impl_;
+    public:
+        ZeroMQComponent();
+        ~ZeroMQComponent();
+        //only host and port are needed in the locators
+        struct NoTopicSelection {};
+        void ZeroMQ_addSubscriptionClient(ConnectionLocator const &locator,
+                        std::variant<NoTopicSelection, std::string, std::regex> const &topic,
+                        std::function<void(basic::ByteDataWithTopic &&)> client,
+                        std::optional<WireToUserHook> wireToUserHook = std::nullopt);
+        std::function<void(basic::ByteDataWithTopic &&)> ZeroMQ_getPublisher(ConnectionLocator const &locator, std::optional<UserToWireHook> userToWireHook = std::nullopt);
+    };
+
+} } } } }
+
+
+#endif
