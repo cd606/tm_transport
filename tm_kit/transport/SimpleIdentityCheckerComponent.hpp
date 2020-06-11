ba7b7d3af7dc2cdff86786b6372fae7dfe35b424
cd606 <64788159+cd606@users.noreply.github.com>
2020-06-11 14:30:18 -0500

    use CBOR for simple identity attaching

--- tm_kit/transport/SimpleIdentityCheckerComponent.hpp
+++ tm_kit/transport/SimpleIdentityCheckerComponent.hpp
@@ -5,22 +5,17 @@
 #include <tm_kit/transport/AbstractIdentityCheckerComponent.hpp>
 #include <sstream>
 #include <cstring>
-#include <boost/endian/conversion.hpp>
 
 namespace dev { namespace cd606 { namespace tm { namespace transport {
 
     template <class Identity, class Request>
     class ClientSideSimpleIdentityAttacherComponent : public ClientSideAbstractIdentityAttacherComponent<Identity,Request> {
     private:
-        std::string serializedIdentity_;
-        uint32_t serializedIdentityLength_;
+        Identity identity_;
     public:
-        ClientSideSimpleIdentityAttacherComponent() : serializedIdentity_(), serializedIdentityLength_(0) {}
+        ClientSideSimpleIdentityAttacherComponent() {}
         ClientSideSimpleIdentityAttacherComponent(Identity const &identity) 
-            : serializedIdentity_(
-                basic::bytedata_utils::RunSerializer<Identity>::apply(identity)
-            )
-            , serializedIdentityLength_(boost::endian::native_to_little<uint32_t>(serializedIdentity_.length()))
+            : identity_(identity)
         {  
         }
         ClientSideSimpleIdentityAttacherComponent(ClientSideSimpleIdentityAttacherComponent const &) = default;
@@ -30,11 +25,14 @@ namespace dev { namespace cd606 { namespace tm { namespace transport {
         ~ClientSideSimpleIdentityAttacherComponent() {}
 
         virtual basic::ByteData attach_identity(basic::ByteData &&d) override final {
-            std::ostringstream oss;
-            oss.write(reinterpret_cast<char const *>(&serializedIdentityLength_), sizeof(uint32_t));
-            oss.write(serializedIdentity_.c_str(), serializedIdentityLength_);
-            oss.write(d.content.c_str(), d.content.length());
-            return {oss.str()};
+            std::tuple<Identity const *, basic::ByteData const *> t {
+                &identity_, &d
+            };
+            return basic::ByteData {
+                basic::bytedata_utils::RunSerializer<
+                    basic::CBOR<std::tuple<Identity const *, basic::ByteData const *>>
+                >::apply({t})
+            };
         }
     };
 
@@ -42,29 +40,14 @@ namespace dev { namespace cd606 { namespace tm { namespace transport {
     class ServerSideSimpleIdentityCheckerComponent : public ServerSideAbstractIdentityCheckerComponent<Identity,Request> {
     public:
         virtual std::optional<std::tuple<Identity, basic::ByteData>> check_identity(basic::ByteData &&d) override final {
-            std::size_t sizeLeft = d.content.length();
-            const char *p = d.content.c_str();
-            if (sizeLeft < sizeof(uint32_t)) {
+            auto t = basic::bytedata_utils::RunDeserializer<
+                basic::CBOR<std::tuple<Identity, basic::ByteData>>
+            >::apply(d.content);
+            if (t) {
+                return t->value;
+            } else {
                 return std::nullopt;
             }
-            uint32_t prefixLen;
-            std::memcpy(&prefixLen, p, sizeof(uint32_t));
-            prefixLen = boost::endian::little_to_native<uint32_t>(prefixLen);
-            sizeLeft -= sizeof(uint32_t);
-            p += sizeof(uint32_t);
-            if (sizeLeft < prefixLen) {
-                return std::nullopt;
-            }
-            std::optional<Identity> identity = basic::bytedata_utils::RunDeserializer<Identity>::apply(
-                std::string(p, p+prefixLen)
-            );
-            if (!identity) {
-                return std::nullopt;
-            }
-            sizeLeft -= prefixLen;
-            p += prefixLen;
-            basic::ByteData content { std::string(p, p+sizeLeft) };
-            return std::tuple<Identity, basic::ByteData> {std::move(*identity), std::move(content)};
         }
     };
 
