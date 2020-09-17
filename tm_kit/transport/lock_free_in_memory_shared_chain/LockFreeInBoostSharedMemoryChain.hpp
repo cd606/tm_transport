1497c93cd56c0c3f28559da7d8790d0041fecc74
cd606 <64788159+cd606@users.noreply.github.com>
2020-09-17 09:54:52 -0500

    boost shared memory chain updates

--- tm_kit/transport/lock_free_in_memory_shared_chain/LockFreeInBoostSharedMemoryChain.hpp
+++ tm_kit/transport/lock_free_in_memory_shared_chain/LockFreeInBoostSharedMemoryChain.hpp
@@ -15,22 +15,25 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
     };
 
     template <class T>
-    struct StorageItem {
+    struct BoostSharedMemoryStorageItem {
         char id[36]; //for uuid, please notice that the end '\0' is not included
         T data;
-        std::atomic<StorageItem<T> *> next;
+        std::atomic<std::ptrdiff_t> next;
+        BoostSharedMemoryStorageItem() : data(), next(0) {
+            std::memset(id, 0, 36);
+        }
     };
     
     template <class T>
-    using ChainItem = StorageItem<T> *;
+    using BoostSharedMemoryChainItem = BoostSharedMemoryStorageItem<T> *;
     
-    template <class T, class Enable=std::enable_if_t<std::is_pod_v<T>, void>>
+    template <class T, class Enable=std::enable_if_t<std::is_standard_layout_v<T>, void>>
     class LockFreeInBoostSharedMemoryChain {
     private:
         boost::interprocess::managed_shared_memory mem_;
-        StorageItem<T> *head_;
+        BoostSharedMemoryStorageItem<T> *head_;
     public:
-        using ItemType = ChainItem<T>;
+        using ItemType = BoostSharedMemoryChainItem<T>;
         LockFreeInBoostSharedMemoryChain(std::string const &name, std::size_t sharedMemorySize) : 
             mem_(
                 boost::interprocess::open_or_create
@@ -39,10 +42,7 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
             )
             , head_(nullptr)
         {
-            head_ = mem_.find_or_construct<StorageItem<T>>("head")();
-            head_->id[0] = '\0';
-            head_->data = T {};
-            head_->next = nullptr;
+            head_ = mem_.find_or_construct<BoostSharedMemoryStorageItem<T>>("head")();
         }
         ItemType head(void *) {
             return head_;
@@ -51,15 +51,15 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
             if (id == "" || id == "head") {
                 return head_;
             }
-            return mem_.find<StorageItem<T>>(id.c_str()).first;
+            return mem_.find<BoostSharedMemoryStorageItem<T>>(id.c_str()).first;
         }
         std::optional<ItemType> fetchNext(ItemType const &current) {
             if (!current) {
                 throw LockFreeInBoostSharedMemoryChainException("FetchNext on nullptr");
             }
-            StorageItem<T> *p = current->next.load(std::memory_order_acquire);
-            if (p) {
-                return p;
+            auto next = current->next.load(std::memory_order_acquire);
+            if (next != 0) {
+                return (current+next);
             } else {
                 return std::nullopt;
             }
@@ -71,37 +71,36 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
             if (!toBeWritten) {
                 throw LockFreeInBoostSharedMemoryChainException("AppendAfter trying to append nullptr");
             }
-            StorageItem<T> *p = nullptr;
-            return std::atomic_compare_exchange_strong<StorageItem<T> *>(
+            if (toBeWritten->next != 0) {
+                throw LockFreeInBoostSharedMemoryChainException("AppendAfter trying to append an item with non-zero next");
+            }
+            std::ptrdiff_t x = 0;
+            return std::atomic_compare_exchange_strong<std::ptrdiff_t>(
                 &(current->next)
-                , &p
-                , toBeWritten
+                , &x
+                , (toBeWritten-current)
             );
         }
         template <class ExtraData>
         void saveExtraData(std::string const &key, ExtraData const &data) {
-            if constexpr (std::is_pod_v<ExtraData>) {
-                *(mem_.find_or_construct<ExtraData>(key.c_str())()) = data;
-            }
+            static_assert(std::is_standard_layout_v<ExtraData>, "LockFreeInBoostSharedMemoryChain only supports storing standard layout extraData objects");
+            *(mem_.find_or_construct<ExtraData>(key.c_str())()) = data;
         }
         template <class ExtraData>
         std::optional<ExtraData> loadExtraData(std::string const &key) {
-            if constexpr (std::is_pod_v<ExtraData>) {
-                auto *p = mem_.find<ExtraData>(key.c_str()).first;
-                if (p) {
-                    return *p;
-                } else {
-                    return std::nullopt;
-                }
+            static_assert(std::is_standard_layout_v<ExtraData>, "LockFreeInBoostSharedMemoryChain only supports loading standard layout extraData objects");
+            auto *p = mem_.find<ExtraData>(key.c_str()).first;
+            if (p) {
+                return *p;
             } else {
                 return std::nullopt;
             }
         }
         ItemType createItemFromData(std::string const &id, T &&data) {
-            auto *p = mem_.construct<StorageItem<T>>(id.c_str())();
-            std::memcpy(p->id, id.c_str(), std::min(36, id.length()));
+            auto *p = mem_.construct<BoostSharedMemoryStorageItem<T>>(id.c_str())();
+            std::memcpy(p->id, id.c_str(), std::min<std::size_t>(36, id.length()));
             p->data = std::move(data);
-            p->next = nullptr;
+            p->next = 0;
             return p;
         }
         void destroyItem(ItemType &p) {
