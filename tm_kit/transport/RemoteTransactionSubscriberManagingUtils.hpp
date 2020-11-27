7d022c5e7c9eca2b2f8dc20f48f8a53d42a6e11d
cd606 <cd606dc@gmail.com>
2020-11-27 16:47:11 -0600

    some reorganization of helper function

--- tm_kit/transport/RemoteTransactionSubscriberManagingUtils.hpp
+++ tm_kit/transport/RemoteTransactionSubscriberManagingUtils.hpp
@@ -4,50 +4,24 @@
 #include <tm_kit/transport/MultiTransportRemoteFacilityManagingUtils.hpp>
 
 namespace dev { namespace cd606 { namespace tm { namespace transport {
-    template <class R, class GS>
+    template <class R>
     class RemoteTransactionSubscriberManagingUtils {
-    public:
-        static auto createSubscriber(
+    private:
+        template <class GS>
+        static auto attachExitSourceToGSClient(
             R &r 
-            , typename R::template ConvertibleToSourceoid<HeartbeatMessage> &&heartbeatSource
-            , std::regex const &serverNameRE
             , std::string const &facilityRegistrationName
-            , typename GS::Subscription &&initialSubscriptionCommand
+            , typename MultiTransportRemoteFacilityManagingUtils<R>
+                ::template DistinguishedRemoteFacility<
+                    typename GS::Input, typename GS::Output
+                > const &gsClient 
             , std::optional<typename R::template Source<basic::VoidStruct>> exitSource = std::nullopt
-            , std::optional<ByteDataHookPair> hooks = std::nullopt
-            , std::chrono::system_clock::duration ttl = std::chrono::seconds(3)
-            , std::chrono::system_clock::duration checkPeriod = std::chrono::seconds(5)
         ) -> typename R::template Sourceoid<
             typename R::AppType::template KeyedData<
                 typename GS::Input, typename GS::Output
             >
         > {
             using M = typename R::AppType;
-            auto res = MultiTransportRemoteFacilityManagingUtils<R>::template setupOneDistinguishedRemoteFacility
-            <typename GS::Input, typename GS::Output>(
-                r 
-                , std::move(heartbeatSource)
-                , serverNameRE
-                , facilityRegistrationName
-                , [cmd=std::move(initialSubscriptionCommand)]() {
-                    return typename GS::Input {
-                        std::move(cmd)
-                    };
-                }
-                , [](typename GS::Input const &, typename GS::Output const &o) {
-                    return std::visit([](auto const &x) -> bool {
-                        using T = std::decay_t<decltype(x)>;
-                        if constexpr (std::is_same_v<T, typename GS::Subscription>) {
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }, o.value);
-                }
-                , hooks 
-                , ttl
-                , checkPeriod
-            );
             if (exitSource) {
                 auto idStorage = std::make_shared<std::unordered_map<ConnectionLocator, typename M::EnvironmentType::IDType>>();
                 auto idStorageMutex = std::make_shared<std::mutex>();
@@ -98,7 +72,7 @@ namespace dev { namespace cd606 { namespace tm { namespace transport {
                     }
                 );
                 r.registerAction("gs:"+facilityRegistrationName+"/saveIDAndRemoveConnector", saveIDAndRemoveConnector);
-                res.feedOrderResults(r, r.actionAsSink(saveIDAndRemoveConnector));
+                gsClient.feedOrderResults(r, r.actionAsSink(saveIDAndRemoveConnector));
                 auto removeID = M::template pureExporter<
                     std::tuple<ConnectionLocator, bool>
                 >(
@@ -112,7 +86,7 @@ namespace dev { namespace cd606 { namespace tm { namespace transport {
                     }
                 );
                 r.registerExporter("gs:"+facilityRegistrationName+"/removeID", removeID);
-                res.feedConnectionChanges(r, r.exporterAsSink(removeID));
+                gsClient.feedConnectionChanges(r, r.exporterAsSink(removeID));
                 auto unsubscribe = M::template kleisliMulti<basic::VoidStruct>(
                     [idStorage,idStorageMutex](typename M::template InnerData<basic::VoidStruct> &&d) -> typename M::template Data<std::vector<typename M::template Key<std::tuple<ConnectionLocator, typename GS::Input>>>>
                     {
@@ -141,7 +115,7 @@ namespace dev { namespace cd606 { namespace tm { namespace transport {
                     }
                 );
                 r.registerAction("gs:"+facilityRegistrationName+"/unsubscribe", unsubscribe);
-                res.orderReceiver(r, r.execute(unsubscribe, exitSource->clone()));
+                gsClient.orderReceiver(r, r.execute(unsubscribe, exitSource->clone()));
                 return r.sourceAsSourceoid(r.actionAsSource(saveIDAndRemoveConnector));
             } else {
                 auto removeConnector = M::template liftPure<
@@ -157,9 +131,121 @@ namespace dev { namespace cd606 { namespace tm { namespace transport {
                     }
                 );
                 r.registerAction("gs:"+facilityRegistrationName+"/removeConnector", removeConnector);
-                res.feedOrderResults(r, r.actionAsSink(removeConnector));
+                gsClient.feedOrderResults(r, r.actionAsSink(removeConnector));
                 return r.sourceAsSourceoid(r.actionAsSource(removeConnector));
             }
+        }
+    public:
+        template <class GS>
+        static auto createSubscriber(
+            R &r 
+            , typename R::template ConvertibleToSourceoid<HeartbeatMessage> &&heartbeatSource
+            , std::regex const &serverNameRE
+            , std::string const &facilityRegistrationName
+            , typename GS::Subscription &&initialSubscriptionCommand
+            , std::optional<typename R::template Source<basic::VoidStruct>> exitSource = std::nullopt
+            , std::optional<ByteDataHookPair> hooks = std::nullopt
+            , std::chrono::system_clock::duration ttl = std::chrono::seconds(3)
+            , std::chrono::system_clock::duration checkPeriod = std::chrono::seconds(5)
+        ) -> typename R::template Sourceoid<
+            typename R::AppType::template KeyedData<
+                typename GS::Input, typename GS::Output
+            >
+        > {
+            using M = typename R::AppType;
+            auto res = MultiTransportRemoteFacilityManagingUtils<R>::template setupOneDistinguishedRemoteFacility
+            <typename GS::Input, typename GS::Output>(
+                r 
+                , std::move(heartbeatSource)
+                , serverNameRE
+                , facilityRegistrationName
+                , [cmd=std::move(initialSubscriptionCommand)]() {
+                    return typename GS::Input {
+                        std::move(cmd)
+                    };
+                }
+                , [](typename GS::Input const &, typename GS::Output const &o) {
+                    return std::visit([](auto const &x) -> bool {
+                        using T = std::decay_t<decltype(x)>;
+                        if constexpr (std::is_same_v<T, typename GS::Subscription>) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }, o.value);
+                }
+                , hooks 
+                , ttl
+                , checkPeriod
+            );
+            return attachExitSourceToGSClient<GS>(
+                r 
+                , facilityRegistrationName
+                , res 
+                , exitSource
+            );
+        }
+        template <class GS, class TI>
+        struct SubscriberAndUpdater {
+            typename R::template Sourceoid<
+                typename R::AppType::template KeyedData<
+                    typename GS::Input, typename GS::Output
+                >
+            > feedSubscriberData;
+            typename R::template FacilitioidConnector<
+                typename TI::Transaction, typename TI::TransactionResponse
+            > connectUpdateRequest;
+        };
+        template <class GS, class TI>
+        static auto createSubscriberAndUpdater(
+            R &r 
+            , typename R::template ConvertibleToSourceoid<HeartbeatMessage> &&heartbeatSource
+            , std::regex const &serverNameRE
+            , std::string const &subscriptionFacilityRegistrationName
+            , std::string const &transactionFacilityRegistrationName
+            , typename GS::Subscription &&initialSubscriptionCommand
+            , std::optional<typename R::template Source<basic::VoidStruct>> exitSource = std::nullopt
+            , std::optional<ByteDataHookPair> subscriptionFacilityHooks = std::nullopt
+            , std::optional<ByteDataHookPair> transactionFacilityHooks = std::nullopt
+            , std::chrono::system_clock::duration ttl = std::chrono::seconds(3)
+            , std::chrono::system_clock::duration checkPeriod = std::chrono::seconds(5)
+        ) -> SubscriberAndUpdater<GS,TI> 
+        {
+            using M = typename R::AppType;
+            auto res = MultiTransportRemoteFacilityManagingUtils<R>::template setupTwoStepRemoteFacility
+            <typename GS::Input, typename GS::Output, typename TI::Transaction, typename TI::TransactionResponse>(
+                r 
+                , std::move(heartbeatSource)
+                , serverNameRE
+                , {subscriptionFacilityRegistrationName, transactionFacilityRegistrationName}
+                , [cmd=std::move(initialSubscriptionCommand)]() {
+                    return typename GS::Input {
+                        std::move(cmd)
+                    };
+                }
+                , [](typename GS::Input const &, typename GS::Output const &o) {
+                    return std::visit([](auto const &x) -> bool {
+                        using T = std::decay_t<decltype(x)>;
+                        if constexpr (std::is_same_v<T, typename GS::Subscription>) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }, o.value);
+                }
+                , {subscriptionFacilityHooks, transactionFacilityHooks}
+                , ttl
+                , checkPeriod
+            );
+            return SubscriberAndUpdater<GS,TI> {
+                attachExitSourceToGSClient<GS>(
+                    r 
+                    , subscriptionFacilityRegistrationName
+                    , std::get<0>(res)
+                    , exitSource
+                )
+                , std::get<1>(res)
+            };
         }
     };
 } } } }
