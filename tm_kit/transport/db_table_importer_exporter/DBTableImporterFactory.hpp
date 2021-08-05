0d22eb3897423a02cfcab1aa2948fb50874e2d11
cd606 <cd606dc@gmail.com>
2021-08-05 11:00:17 -0500

    more sql options for db table importer

--- tm_kit/transport/db_table_importer_exporter/DBTableImporterFactory.hpp
+++ tm_kit/transport/db_table_importer_exporter/DBTableImporterFactory.hpp
@@ -4,24 +4,40 @@
 #include <tm_kit/infra/AppClassifier.hpp>
 #include <tm_kit/basic/StructFieldInfoUtils.hpp>
 
+#include <boost/algorithm/string.hpp>
+
 #include <soci/soci.h>
 
 namespace dev { namespace cd606 { namespace tm { namespace transport { namespace db_table_importer_exporter {
     template <class M>
     class DBTableImporterFactory {
+    private:
+        template <class T>
+        static std::string selectStatement(std::string const &input) {
+            using DF = basic::struct_field_info_utils::StructFieldInfoBasedDataFiller<T>;
+            std::string s = boost::trim_copy(input);
+            std::string s1 = boost::to_upper_copy(s);
+            if (boost::starts_with(s1, "SELECT ")) {
+                return s;
+            }
+            if (boost::starts_with(s1, "FROM ")) {
+                return ("SELECT "+DF::commaSeparatedFieldNames()+" "+s);
+            }
+            return ("SELECT "+DF::commaSeparatedFieldNames()+" FROM "+s);
+        }
     public:
         template <class T, typename = std::enable_if_t<basic::StructFieldInfo<T>::HasGeneratedStructFieldInfo>>
-        static auto createImporter(std::shared_ptr<soci::session> const &session, std::string const &tableName)
+        static auto createImporter(std::shared_ptr<soci::session> const &session, std::string const &importerInput)
             -> std::shared_ptr<typename M::template Importer<std::vector<T>>>
         {
             return M::template uniformSimpleImporter<std::vector<T>>(
-                [session,tableName](
+                [session,importerInput](
                     typename M::EnvironmentType *env
                 ) -> std::tuple<bool, typename M::template Data<std::vector<T>>> {
                     using DF = basic::struct_field_info_utils::StructFieldInfoBasedDataFiller<T>;
                     std::vector<T> value;
                     soci::rowset<soci::row> queryRes = 
-                        session->prepare << ("SELECT "+DF::commaSeparatedFieldNames()+" FROM "+tableName);
+                        session->prepare << selectStatement<T>(importerInput);
                     for (auto const &r : queryRes) {
                         value.push_back(DF::retrieveData(r,0));
                     }
@@ -40,12 +56,12 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
             );
         }
         template <class T, typename = std::enable_if_t<basic::StructFieldInfo<T>::HasGeneratedStructFieldInfo>>
-        static auto createRepeatedImporter(std::shared_ptr<soci::session> const &session, std::string const &tableName, std::chrono::system_clock::duration const &interval)
+        static auto createRepeatedImporter(std::shared_ptr<soci::session> const &session, std::string const &importerInput, std::chrono::system_clock::duration const &interval)
             -> std::shared_ptr<typename M::template Importer<std::vector<T>>>
         {
             static_assert((infra::app_classification_v<M> == infra::AppClassification::RealTime), "repeated db table importer is only supported in real time mode");
             return M::template uniformSimpleImporter<std::vector<T>>(
-                [session,tableName,interval](
+                [session,importerInput,interval](
                     typename M::EnvironmentType *env
                 ) -> std::tuple<bool, typename M::template Data<std::vector<T>>> {
                     using DF = basic::struct_field_info_utils::StructFieldInfoBasedDataFiller<T>;
@@ -57,7 +73,7 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
                     try {
                         std::vector<T> value;
                         soci::rowset<soci::row> queryRes = 
-                            session->prepare << ("SELECT "+DF::commaSeparatedFieldNames()+" FROM "+tableName);
+                            session->prepare << selectStatement<T>(importerInput);
                         for (auto const &r : queryRes) {
                             value.push_back(DF::retrieveData(r,0));
                         }
@@ -79,12 +95,12 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
             );
         }
         template <class T, class KeyExtractor, typename = std::enable_if_t<basic::StructFieldInfo<T>::HasGeneratedStructFieldInfo>>
-        static auto createRepeatedImporterWithKeyCheck(std::shared_ptr<soci::session> const &session, std::string const &tableName, std::chrono::system_clock::duration const &interval, KeyExtractor &&keyExtractor)
+        static auto createRepeatedImporterWithKeyCheck(std::shared_ptr<soci::session> const &session, std::string const &importerInput, std::chrono::system_clock::duration const &interval, KeyExtractor &&keyExtractor)
             -> std::shared_ptr<typename M::template Importer<std::vector<T>>>
         {
             static_assert((infra::app_classification_v<M> == infra::AppClassification::RealTime), "repeated db table importer with key check is only supported in real time mode");
             return M::template uniformSimpleImporter<std::vector<T>>(
-                [session,tableName,interval,keyExtractor=std::move(keyExtractor)](
+                [session,importerInput,interval,keyExtractor=std::move(keyExtractor)](
                     typename M::EnvironmentType *env
                 ) mutable -> std::tuple<bool, typename M::template Data<std::vector<T>>> {
                     using DF = basic::struct_field_info_utils::StructFieldInfoBasedDataFiller<T>;
@@ -99,7 +115,7 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
                     try {
                         std::vector<T> value;
                         soci::rowset<soci::row> queryRes = 
-                            session->prepare << ("SELECT "+DF::commaSeparatedFieldNames()+" FROM "+tableName);
+                            session->prepare << selectStatement<T>(importerInput);
                         for (auto const &r : queryRes) {
                             auto d = DF::retrieveData(r, 0);
                             auto k = keyExtractor(d);
