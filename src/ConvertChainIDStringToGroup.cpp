95e6d8e9485a33f39dbd6f4e97f33b2fc9f0e179
cd606 <cd606dc@gmail.com>
2021-11-02 20:13:14 -0500

    add a utility to create groups out of chain ID

---
+++ src/ConvertChainIDStringToGroup.cpp
@@ -0,0 +1,38 @@
+#include <tm_kit/transport/ConvertChainIDStringToGroup.hpp>
+
+#include <sodium/crypto_generichash.h>
+
+namespace dev { namespace cd606 { namespace tm { namespace transport {
+    namespace chain_utils {
+        uint32_t convertChainIDStringToGroup(std::string_view const &id, uint32_t totalGroups) {
+            static_assert((crypto_generichash_BYTES % 4 == 0), "generic hash bytes must be a multiple of 4");
+            if (totalGroups == 0) {
+                return 0;
+            }
+            unsigned char hash[crypto_generichash_BYTES];
+            crypto_generichash(
+                hash, crypto_generichash_BYTES 
+                , reinterpret_cast<const unsigned char *>(id.data())
+                , id.length()
+                , nullptr, 0
+            );
+            uint32_t ret = 0;
+            if (totalGroups <= 256) {
+                for (int ii=0; ii<crypto_generichash_BYTES; ++ii) {
+                    ret = ret ^ ((uint32_t) hash[ii]);
+                }
+            } else if (totalGroups <= 65536) {
+                for (int ii=0; ii<crypto_generichash_BYTES; ii+=2) {
+                    uint32_t x = (uint32_t) *(reinterpret_cast<const uint16_t *>(&hash[ii]));
+                    ret = ret ^ x;
+                }
+            } else {
+                for (int ii=0; ii<crypto_generichash_BYTES; ii+=4) {
+                    uint32_t x = *(reinterpret_cast<const uint32_t *>(&hash[ii]));
+                    ret = ret ^ x;
+                }
+            }
+            return (ret % totalGroups);
+        }
+    }
+} } } }
---
+++ tm_kit/transport/ConvertChainIDStringToGroup.hpp
@@ -0,0 +1,12 @@
+#ifndef TM_KIT_TRANSPORT_CONVERT_CHAIN_ID_STRING_TO_GROUP_HPP_
+#define TM_KIT_TRANSPORT_CONVERT_CHAIN_ID_STRING_TO_GROUP_HPP_
+
+#include <string_view>
+
+namespace dev { namespace cd606 { namespace tm { namespace transport {
+    namespace chain_utils {
+        extern uint32_t convertChainIDStringToGroup(std::string_view const &id, uint32_t totalGroups);
+    }
+} } } }
+
+#endif
