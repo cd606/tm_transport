bc599b3901ed1a1f23673500a48f1a374f9cccec
cd606 <cd606dc@gmail.com>
2022-05-24 20:28:34 -0500

    bcl decimal compat bug fix

--- tm_kit/transport/bcl_compat/Decimal.hpp
+++ tm_kit/transport/bcl_compat/Decimal.hpp
@@ -26,11 +26,18 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
         }
         template <class BclDecimalProtoType>
         static void write(BclDecimalProtoType &d, boost::multiprecision::cpp_dec_float_100 const &input) {
+            static const double maxIntRep = std::pow(2.0, 96.0);
             bool isNeg = (input < 0);
             uint32_t exp = 0;
             boost::multiprecision::cpp_dec_float_100 x = (isNeg?-input:input);
             boost::multiprecision::int128_t intRep = x.convert_to<boost::multiprecision::int128_t>();
-            for (; exp<100; ++exp) {
+            uint32_t expCap = (uint32_t) (
+                std::log10(maxIntRep/(double) x)
+            );
+            if (expCap > 100) {
+                expCap = 100;
+            }
+            for (; exp<expCap; ++exp) {
                 if (x == intRep.convert_to<boost::multiprecision::cpp_dec_float_100>()) {
                     break;
                 }
@@ -38,7 +45,7 @@ namespace dev { namespace cd606 { namespace tm { namespace transport { namespace
                 intRep = x.convert_to<boost::multiprecision::int128_t>();
             }
             d.set_lo((uint64_t) (intRep & (uint64_t) 0xffffffffffffffff));
-            d.set_hi((uint32_t) ((intRep >> 64) && (uint32_t) 0xffffffff));
+            d.set_hi((uint32_t) ((intRep >> 64) & (uint32_t) 0xffffffff));
             d.set_signscale(
                 (exp << 1) | (isNeg?1:0)
             );
